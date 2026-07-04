a01c66e92a15423a77b797204dade753e325d32f
LiamTexasTurner <liamtexasturner@gmail.com>
2026-07-04 13:17:34 +1000

    window and game loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,92 @@
-﻿#include <iostream>
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+bool full_screen = false;
+
+unsigned int SCR_WIDTH = 1920;
+unsigned int SCR_HEIGHT = 1080;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void process_input(GLFWwindow *window);
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+      glfwInit();
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);     
+
+      GLFWmonitor* monitor = nullptr;
+      const GLFWvidmode* mode = nullptr;
+      GLFWwindow* window = nullptr;
+
+      //set full screen
+      if(full_screen) 
+      {
+            monitor = glfwGetPrimaryMonitor();
+            mode = glfwGetVideoMode(monitor);
+            SCR_WIDTH = mode->width;
+            SCR_HEIGHT = mode->height;
+            window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", monitor, NULL);
+      }
+      else
+      {
+            window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", monitor, NULL);
+      }
+            
+      
+      if (window == NULL)
+      {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            return -1;
+      }
+      glfwMakeContextCurrent(window);
+      glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+      glfwSetCursorPosCallback(window, mouse_callback);
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+      if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+      {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            return -1;
+      }
+
+      glfwSwapInterval(0);
+
+
+      while(!glfwWindowShouldClose(window))
+      {
+            glfwPollEvents();
+            process_input(window);
+
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            
+            glfwSwapBuffers(window);
+      }
+
+      
+      return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+      glViewport(0, 0, width, height);
+}
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+      
+}
+
+void process_input(GLFWwindow *window)
+{
+      if (glfwGetKey(window, GLFW_KEY_EQUAL) == GLFW_PRESS)
+      {
+            glfwSetWindowShouldClose(window, true);
+      }
 }
